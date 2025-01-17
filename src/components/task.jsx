a69afdaf5c75411b0fa424c8f29d8d6dfd171456
Dexi22 <den17.lub@gmail.com>
2025-01-17 13:19:25 +0300

    Increase tile size

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,6 +1,6 @@
 export default function ({ name, editMode, tabIndex, ref, onSubmit }) {
 	const sharedClasses =
-		'bg-primary focus:outline outline-[#9a68ea] w-full text-left rounded-full p-2 focus:-translate-x-1 transition-all hover:bg-[#291a5c] focus:bg-[#291a5c]';
+		'bg-primary focus:outline outline-[#9a68ea] w-full text-left rounded-full p-3 focus:-translate-x-1 transition-all hover:bg-[#291a5c] focus:bg-[#291a5c]';
 
 	return (
 		<li className="rounded-xl">
